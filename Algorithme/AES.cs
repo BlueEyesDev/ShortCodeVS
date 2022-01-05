7f50c15d0908fa44ff61238a9e68e411878439f9
BlueEyesDev <84388093+BlueEyesDev@users.noreply.github.com>
2022-01-05 11:40:57 +0100

    Create AES.cs

---
+++ Algorithme/AES.cs
@@ -0,0 +1,32 @@
+public static (string Tostring, byte[] Bytes) AES(object Data, bool encrypt, byte[] SetKey = null, byte[] SetIv = null) {
+            try
+            {
+				if (Data is byte[] == false && (((string)Data).Length % 4 == 0) && Regex.IsMatch((string)Data, @"^[a-zA-Z0-9\+/]*={0,3}$", RegexOptions.None))
+					Data = Convert.FromBase64String((string)Data);
+				AesManaged aes = new AesManaged();
+				aes.Key = (SetKey != null) ? SetKey : aes.Key;
+				aes.IV = (SetIv != null) ? SetIv : aes.IV;
+				if (encrypt)
+				{
+					using (MemoryStream ms = new MemoryStream())
+					{
+						using (CryptoStream cs = new CryptoStream(ms, aes.CreateEncryptor(aes.Key, aes.IV), CryptoStreamMode.Write))
+						using (StreamWriter sw = new StreamWriter(cs))
+							sw.Write((string)Data);
+						return (Convert.ToBase64String(ms.ToArray()), ms.ToArray());
+					}
+				}
+				else
+				{
+					using (MemoryStream ms = new MemoryStream((byte[])Data))
+					using (CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(aes.Key, aes.IV), CryptoStreamMode.Read))
+					using (StreamReader reader = new StreamReader(cs))
+						Data = reader.ReadToEnd();
+					return ((string)Data, UTF8Encoding.UTF8.GetBytes((string)Data));
+				}
+			}
+            catch (Exception ex) {
+
+                throw ex;
+            }
+		}
